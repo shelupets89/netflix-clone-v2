e80be74b48bce6c1752da6293a27cb1b87612782
Dmitry Shelupets <shelupets89@gmail.com>
2022-09-30 17:52:16 +0300

    replace to screens

---
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import './HomeScreen.scss'
+
+const HomeScreen = () => {
+  return <div className='homeScreen'>
+    <Navbar/>
+    {/* banner */}
+    {/* row */}
+  </div>
+}
+
+export default HomeScreen
