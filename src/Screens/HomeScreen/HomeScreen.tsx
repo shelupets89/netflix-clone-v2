8c0aaf72f7626166bf103a694002ef8e3f18f69b
Dmitry Shelupets <shelupets89@gmail.com>
2022-09-30 19:50:32 +0300

    HomeScreen add banner

--- src/Screens/HomeScreen/HomeScreen.tsx
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Banner from '../../components/Banner'
 import Navbar from '../../components/Navbar'
 import './HomeScreen.scss'
 
 const HomeScreen = () => {
-  return <div className='homeScreen'>
-    <Navbar/>
-    {/* banner */}
-    {/* row */}
-  </div>
+  return (
+    <div className='homeScreen'>
+      <Navbar />
+      <Banner />
+      {/* banner */}
+      {/* row */}
+    </div>
+  )
 }
 
 export default HomeScreen
