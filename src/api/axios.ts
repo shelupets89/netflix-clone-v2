f0d89690968fdc58413c0a8a91d1a5530a49b73a
Dmitry Shelupets <shelupets89@gmail.com>
2022-09-30 20:07:56 +0300

    axios init

---
+++ src/api/axios.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import { BASE_URI_TMDB } from '../constants/api'
+
+const instance = axios.create({
+    baseURL: BASE_URI_TMDB
+})
+
+export default instance
---
+++ src/constants/api.ts
@@ -0,0 +1,1 @@
+export const BASE_URI_TMDB = 'https://api.themoviedb.org/3'
