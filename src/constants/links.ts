82b076cdeba3155bee700df67385c2f8e7157c4a
Dmitry Shelupets <shelupets89@gmail.com>
2022-09-30 19:49:53 +0300

    links update

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,3 +1,7 @@
-export const netflixLogoBig = 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png';
-export const netflixLogoIcon = 'https://cdn.icon-icons.com/icons2/3053/PNG/512/netflix_macos_bigsur_icon_189917.png';
-export const netflixAvatar = 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+export const netflixLogoBig = 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'
+export const netflixLogoIcon =
+  'https://cdn.icon-icons.com/icons2/3053/PNG/512/netflix_macos_bigsur_icon_189917.png'
+export const netflixAvatar =
+  'https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+export const netflixBanner =
+  'https://raw.githubusercontent.com/thatanjan/netflix-clone-yt/youtube/media//banner.jpg'
