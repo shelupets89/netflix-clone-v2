c3fdf7c7f2b283ac2f04c9c30dfa51396f11b719
Dmitry Shelupets <shelupets89@gmail.com>
2022-09-30 18:17:36 +0300

    links const

---
+++ src/constants/links.ts
@@ -0,0 +1,3 @@
+export const netflixLogoBig = 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png';
+export const netflixLogoIcon = 'https://cdn.icon-icons.com/icons2/3053/PNG/512/netflix_macos_bigsur_icon_189917.png';
+export const netflixAvatar = 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
