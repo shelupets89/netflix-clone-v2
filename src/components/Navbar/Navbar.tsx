efc804748110c0aa42861dcc8c72536dff27615e
Dmitry Shelupets <shelupets89@gmail.com>
2022-09-30 18:30:00 +0300

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { netflixAvatar, netflixLogoBig } from '../../constants/links'
 import './Navbar.scss'
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>Navbar</div>
-  )
+    const [show, setShow] = useState<boolean>(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', transitionNavBar)
+        return () => window.removeEventListener('scroll', transitionNavBar)
+    }, [])
+
+    return (
+        <div className={`nav ${show && 'nav_black'}`}>
+            <div className="nav__contents">
+                <img className='nav__logo' src={netflixLogoBig} alt="netflix logo" />
+                <img className='nav__avatar' src={netflixAvatar} alt="netflix avatar" />
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
