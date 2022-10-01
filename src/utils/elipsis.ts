b279aaeb7c1ffc8f5df399f0a153b664b6465eba
Dmitry Shelupets <shelupets89@gmail.com>
2022-10-01 13:07:56 +0300

    utils update

--- src/utils/elipsis.ts
+++ src/utils/elipsis.ts
@@ -1,5 +1,8 @@
-const elipsis = (str: string, num: number): string => {
-  return str?.length > num ? str.substring(0, num - 1) + '...' : str
+const elipsis = (str: string | undefined, num: number): string => {
+  if (str) {
+    return str?.length > num ? str.substring(0, num - 1) + '...' : str
+  }
+  return '';
 }
 
 export default elipsis
