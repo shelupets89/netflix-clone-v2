6d724cb140d27d6c70cd1b101f7a1d6240be7f42
Dmitry Shelupets <shelupets89@gmail.com>
2022-10-01 19:41:39 +0300

    RowItem update

--- src/components/RowItem/RowItem.tsx
+++ src/components/RowItem/RowItem.tsx
@@ -3,18 +3,22 @@ import Movie from '../../types/Movie'
 import './RowItem.scss'
 
 type Props = {
-    movie: Movie
-    isLargeRow: boolean
+  movie: Movie
+  isLargeRow: boolean
 }
 
 const RowItem = ({ movie, isLargeRow }: Props) => {
-    return (
-        <img
-            className={`rowItem__poster ${isLargeRow && 'rowItem__posterLarge'}`}
-            src={isLargeRow ? movie.backdrop_path : movie.poster_path}
-            alt={movie.original_name}
-        />
-    )
+  const posterSrc = movie.poster_path
+  const backdropSrc = movie.backdrop_path
+  const imgSrc = isLargeRow ? posterSrc : backdropSrc
+
+  return (
+    <img
+      className={`rowItem__poster ${isLargeRow && 'rowItem__posterLarge'}`}
+      src={imgSrc}
+      alt={movie.original_name}
+    />
+  )
 }
 
 export default RowItem
