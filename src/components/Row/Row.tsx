41acccbe844dd4fae4a94ce97f586e7d35d92b87
Dmitry Shelupets <shelupets89@gmail.com>
2022-10-01 19:41:18 +0300

    row empty img filter

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -32,8 +32,15 @@ const Row = ({ title, fetchUri, fetchParams }: Props) => {
         })
         const dataMovies = request.data.results
         const res = dataMovies.map((item: Movie) => {
-          const poster_path = `${IMG_URI_TMDB}${item.poster_path}` as string
-          const backdrop_path = `${IMG_URI_TMDB}${item.backdrop_path}` as string
+          let poster_path = `${IMG_URI_TMDB}${item.poster_path}` as string
+          let backdrop_path = `${IMG_URI_TMDB}${item.backdrop_path}` as string
+
+          if (!item.backdrop_path) {
+            backdrop_path = ''
+          }
+          if (!item.poster_path) {
+            poster_path = ''
+          }
           return { ...item, poster_path, backdrop_path }
         })
         setMovies(res)
@@ -47,14 +54,20 @@ const Row = ({ title, fetchUri, fetchParams }: Props) => {
 
   return (
     <div className='row'>
-      <h2 className='row__title'>{t(title).toUpperCase()}</h2>
-      {movies?.map((movie: Movie) => (
-        <RowItem
-          key={movie.id}
-          movie={movie}
-          isLargeRow={title === TitleKeys.top_rated}
-        />
-      ))}
+      <h2 className='row__title'>{t(title)}</h2>
+      <div className='row__posters'>
+        {movies
+          ?.filter(
+            (movie) => movie.backdrop_path !== '' || movie.poster_path !== '',
+          )
+          .map((movie: Movie) => (
+            <RowItem
+              key={movie.id}
+              movie={movie}
+              isLargeRow={title === TitleKeys.originals}
+            />
+          ))}
+      </div>
     </div>
   )
 }
