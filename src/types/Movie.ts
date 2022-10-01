b9a395150515b707867907cdd996ebf29b8c8c23
Dmitry Shelupets <shelupets89@gmail.com>
2022-10-01 19:03:47 +0300

    movie type

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -12,6 +12,11 @@ interface Movie {
   poster_path: string
   vote_average: number
   vote_count: number
+  adult?: boolean;
+  original_title?: string;
+  release_date?: string;
+  title?: string;
+  video?: boolean;
 }
 
 export default Movie
