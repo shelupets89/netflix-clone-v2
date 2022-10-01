7794bf96b9210daa50d8e3fbc1032e0b2e151ba9
Dmitry Shelupets <shelupets89@gmail.com>
2022-10-01 13:08:26 +0300

    Banner fetch

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+/* eslint-disable camelcase */
+import React, { useEffect, useState } from 'react'
+import axios from '../../api/axios'
+import requests from '../../api/requests'
+import { IMG_URI_TMDB } from '../../constants/api'
 
-import { NFX_BANNER } from '../../constants/links'
 import useTranslation from '../../hooks/useCustomTranslation'
+import Movie from '../../types/Movie'
 import elipsis from '../../utils/elipsis'
 import './Banner.scss'
 
 const Banner = () => {
   const { t } = useTranslation('banner')
+  const [movie, setMovie] = useState<Movie>()
+  const title = movie?.name || movie?.original_name
+  const description = elipsis(movie?.overview, 150)
+  const backgroundImage = `url(${movie?.poster_path})`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals.path, { params: requests.fetchNetflixOriginals.params })
+        const dataArr = request.data.results;
+        const randomIndex = Math.floor(Math.random() * dataArr.length - 1)
+        const resMovie: Movie = dataArr.at(randomIndex) as Movie;
+        const poster_path = `${IMG_URI_TMDB}${resMovie.poster_path}` as string;
+        setMovie({ ...resMovie, poster_path })
+        return request
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [])
+
 
   return (
     <header
@@ -14,16 +40,16 @@ const Banner = () => {
       style={{
         backgroundSize: 'cover',
         backgroundPosition: 'center center',
-        backgroundImage: `url(${NFX_BANNER})`,
+        backgroundImage,
       }}>
       <div className='banner__content'>
-        <h1 className='banner__title'>{t('title')}</h1>
+        <h1 className='banner__title'>{title}</h1>
         <div className='banner__buttons'>
           <button className='banner__btn'>{t('btn_play')}</button>
           <button className='banner__btn'>{t('btn_list')}</button>
         </div>
         <h1 className='banner__description'>
-          {elipsis(t('description'), 150)}
+          {description}
         </h1>
       </div>
 
