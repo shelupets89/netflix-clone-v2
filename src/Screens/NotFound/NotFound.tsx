c5187b84440500c058ef699a66922b558cfa5819
Dmitry Shelupets <shelupets89@gmail.com>
2022-10-01 20:57:21 +0300

    404 page

---
+++ src/Screens/NotFound/NotFound.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { NOT_FOUND } from '../../constants/links';
+import Routes from '../../enums/Routes';
+
+import './NotFound.scss'
+
+const NotFound = () => {
+    const backgroundImage = `url(${NOT_FOUND})`
+
+  return (
+    <div className='notFound' style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundImage
+    }}>
+      <p>
+        <Link to={Routes.home}>Go to the home page</Link>
+      </p>
+    </div>
+  )
+}
+
+export default NotFound
